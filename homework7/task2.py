c189cb6ddddd40f3642e94fa228dcc2a5f8c52b4
NightCatrin <ektant9@gmail.com>
2023-05-26 15:51:34 +0700

    solved the second task

---
+++ homework7/task2.py
@@ -0,0 +1,109 @@
+# Напишите класс PersonInfo
+# Экземпляр класса создается из следующих атрибутов:
+# 1. Строка - "Имя Фамилия"
+# 2. Число - возраст сотрудника
+# 3. Подразделения от головного до того, где работает сотрудник.
+# Реализуйте методы класса:
+# 1. short_name, который возвращает строку Фамилия И.
+# 2. path_deps, возвращает путь "Головное подразделение --> ... --> Конечное подразделение"
+# 3. new_salary, Директор решил проиндексировать зарплаты, и новая зарпалата теперь вычисляет по формуле:
+# 1337*Возраст*суммарное кол-во вхождений трех наиболее часто встречающихся букв из списка подразделений
+# (регистр имеет значение "А" и "а" - разные буквы)
+# Например (Ввод --> Вывод) :
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').short_name() --> 'Шленский А.'
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').path_deps() -->
+#            'Разработка --> УК --> Автотесты'
+# PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты').new_salary() --> 385056 т.к.
+# т.к. буква "т" встречается 4 раза, "а" 3 раза, 'о' 2 раза, остальные по одной. Сумма трёх самых частых букв 4+3+2 = 9.
+# 1337*32*9 = 385056
+
+# Здесь пишем код
+class PersonInfo:
+
+    INDEX = 1337
+
+    def __init__(self, name: str, age: int, *subdivision):
+        self.name = name
+        self.age = age
+        self.subdivision = subdivision
+        self.shortname = ''
+
+    def short_name(self):
+        """Метод возвращает строку 'Фамилия И.'"""
+
+        name1 = self.name.split()
+        self.shortname = name1[1] + ' ' + name1[0][0] + '.'
+
+        return self.shortname
+
+    def path_deps(self):
+        """Метод возвращает путь от головного подразделения до того, где работает сотрудник"""
+
+        return ' --> '.join(self.subdivision)
+
+    def new_salary(self):
+        """Метод для вычисления индексации зарплаты.
+            Соединяем в одну строку все значения subdivision
+            Создаем словарь 'ключ' - Буква,
+            'значение' - количество повторений этой буквы в строке string
+            Суммируем числа трех самых частых букв
+        """
+
+        string = ''.join(self.subdivision)
+        letter_count = {}
+        index = 0
+
+        for letter in string:
+            count = string.count(letter)
+            letter_count[letter] = count
+
+        sorted_dict = sorted(letter_count.values())
+
+        for i in range(1, 4):
+            index += sorted_dict[-i]
+
+        return self.INDEX * self.age * index
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+first_person = PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты')
+fourth_person = PersonInfo('Иван Иванов', 26, 'Разработка')
+second_person = PersonInfo('Пётр Валерьев', 47, 'Разработка', 'УК')
+third_person = PersonInfo('Макар Артуров', 51, 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование')
+
+data = [first_person.short_name,
+        second_person.short_name,
+        third_person.short_name,
+        fourth_person.short_name,
+
+        first_person.path_deps,
+        second_person.path_deps,
+        third_person.path_deps,
+        fourth_person.path_deps,
+
+        first_person.new_salary,
+        second_person.new_salary,
+        third_person.new_salary,
+        fourth_person.new_salary
+        ]
+
+
+test_data = ['Шленский А.', 'Валерьев П.', 'Артуров М.', 'Иванов И.',
+
+             'Разработка --> УК --> Автотесты',
+             'Разработка --> УК',
+             'Разработка --> УК --> Нефункциональное тестирование --> Автотестирование',
+             'Разработка',
+             385056, 314195, 1227366, 173810]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
